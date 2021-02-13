420330b6c78cf979a70cd6d1a4e15ed0914d3d3e
Geoff Coyner <geoffc@qualtrics.com>
2021-02-13 13:48:22 -0800

    added changes to generlize vecotr

--- vector/vector.py
+++ vector/vector.py
@@ -1,10 +1,12 @@
 class Vector:
 
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    def __init__(self, **coords):
+        self.__dict__.update(coords)
 
     def __repr__(self):
-        return "{}({}, {})".format(
-            self.__class__.__name__, self.x, self.y
-        )
+        return "{}({})".format(
+            self.__class__.__name__,
+            ', '.join("{k}={v}".format(
+                k=k,
+                v=self.__dict__[k])
+                for k in sorted(self.__dict__.keys())))
