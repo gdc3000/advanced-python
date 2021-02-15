736fead11fd8d0d72fbf6374dcf2f54b404d3f3c
Geoff Coyner <geoffc@qualtrics.com>
2021-02-15 12:58:16 -0800

    added positive class to planets method. Added regular @property decorators back to name to demo regular decorator approach.

--- planets/planet.py
+++ planets/planet.py
@@ -1,4 +1,24 @@
 
+from weakref import WeakKeyDictionary
+
+class Positive:
+	
+	def __init__(self):
+		self._instance_data = Weak
+	
+	#Descriptor protocol section
+	def __get__(self, instance, owner):
+		return self._instance_data[instance]
+
+	def __set__(self, instance, value):
+		if value <= 0:
+			raise ValueError("Value {} is not positive".format(value))
+		self._instance_data[instance] = value
+
+	def __delete__(self, instance):
+		raise AttributeError("Cannot delete attribute")
+
+
 class Planet:
 
     def __init__(self,
@@ -13,53 +33,17 @@ class Planet:
         self.orbital_period_secs = orbital_period_secs
         self.surface_temp_k = surface_temp_k
 
-    
-    def _get_name(self):
+    @property
+    def name(self):
         return self._name
 
-    def _set_name(self, value):
+    @property.setter
+    def name(self, value):
         if not value:
             raise ValueError("Cannot set empty Planet.name")
         self._name = value
 
-    name = property(fget=_get_name, fset=_set_name)
-
-    def _get_radius_metres(self):
-        return self._radius_metres
-
-    def _set_radius_metres(self, value):
-        if value <= 0:
-            raise ValueError("radius_metres value {} is not positive.".format(value))
-        self._radius_metres = value
-
-    radius_metres = property(fget=_get_radius_metres, fset=_set_radius_metres)
-    
-    def _get_mass_kg(self):
-        return self._mass_kg
-    
-    def _set_mass_kg(self, value):
-        if value <= 0:
-            raise ValueError("mass_kg value {} is not positive.".format(value))
-        self._mass_kg = value
-    
-    mass_kg = property(fget=_get_mass_kg, fset=_set_mass_kg)
-    
-    def _get_orbital_period_secs(self):
-        return self._orbital_period_secs
-    
-    def _set_orbital_period_secs(self, value):
-        if value <= 0:
-            raise ValueError("orbital_period_secs value {} is not positive.".format(value))
-        self._orbital_period_secs = value
-    
-    orbital_period_secs = property(fget=_get_orbital_period_secs, fset=_set_orbital_period_secs)
-
-    def _get_surface_temp_k(self):
-        return self._surface_temp_k
-
-    def _set_surface_temp_k(self, value):
-        if value <= 0:
-            raise ValueError("surface_temp_k value {} is not positive.".format(value))
-        self._surface_temp_k = value
-    
-    surface_temp_k = property(fget=_get_surface_temp_k, fset=_set_surface_temp_k)
+    radius_metres = Positive()
+    mass_kg = Positive()
+    orbital_period_secs = Positive()
+    surface_temp_k = Positive()
