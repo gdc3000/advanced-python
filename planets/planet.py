980a231335192f48e5ab31fdd03cc69dc55acbbd
Geoff Coyner <geoffc@qualtrics.com>
2021-02-22 12:35:01 -0800

    updated planets example to include metaclass to fix error messages. Add metainheritance example.

--- planets/planet.py
+++ planets/planet.py
@@ -1,12 +1,18 @@
 
 from weakref import WeakKeyDictionary
 
-class Positive:
+class Named:
+
+    def __init__(self, name=None):
+        self.name = name
+
+
+class Positive(Named):
     
-    def __init__(self):
+    def __init__(self, name=None):
+        super().__init__(name)
         self._instance_data = WeakKeyDictionary()
     
-    #Descriptor protocol section
     def __get__(self, instance, owner):
         if instance is None:
             return self
@@ -14,26 +20,36 @@ class Positive:
 
     def __set__(self, instance, value):
         if value <= 0:
-            raise ValueError("Value {} is not positive".format(value))
+            raise ValueError("Attribute value {} {} is not positive".format(self.name, value))
         self._instance_data[instance] = value
 
     def __delete__(self, instance):
         raise AttributeError("Cannot delete attribute")
 
 
-class Planet:
+class DescriptorNamingMeta(type):
+
+    def __new__(mcs, name, bases, namespace):
+        for name, attr in namespace.items():
+            if isinstance(attr, Named):
+                attr.name = name
+        return super().__new__(mcs, name, bases, namespace)
+
+
+
+class Planet(metaclass=DescriptorNamingMeta):
 
     def __init__(self,
                 name,
                 radius_metres,
-                mass_kg,
-                orbital_period_secs,
-                surface_temp_k):
+                mass_kilograms,
+                orbital_period_seconds,
+                surface_temperature_kelvin):
         self.name = name
         self.radius_metres = radius_metres
-        self.mass_kg = mass_kg
-        self.orbital_period_secs = orbital_period_secs
-        self.surface_temp_k = surface_temp_k
+        self.mass_kilograms = mass_kilograms
+        self.orbital_period_seconds = orbital_period_seconds
+        self.surface_temperature_kelvin = surface_temperature_kelvin
 
     @property
     def name(self):
@@ -46,6 +62,38 @@ class Planet:
         self._name = value
 
     radius_metres = Positive()
-    mass_kg = Positive()
-    orbital_period_secs = Positive()
-    surface_temp_k = Positive()
+    mass_kilograms = Positive()
+    orbital_period_seconds = Positive()
+    surface_temperature_kelvin = Positive()
+
+
+def make_planets():
+
+    mercury = Planet("Mercury",
+                     radius_metres=2439.7e3,
+                     mass_kilograms=3.3022e23,
+                     orbital_period_seconds=7.60052e6,
+                     surface_temperature_kelvin=340)
+
+    venus = Planet("Venus",
+                   radius_metres=6051.8e3,
+                   mass_kilograms=4.8676e24,
+                   orbital_period_seconds=1.94142e7,
+                   surface_temperature_kelvin=737)
+
+    earth = Planet("Earth",
+                   radius_metres=6371.0e3,
+                   mass_kilograms=5.972e24,
+                   orbital_period_seconds=3.15581e7,
+                   surface_temperature_kelvin=288)
+
+    mars = Planet("Mars",
+                  radius_metres=3389.5e3,
+                  mass_kilograms=6.4185e23,
+                  orbital_period_seconds=5.93543e7,
+                  surface_temperature_kelvin=210)
+
+    return mercury, venus, earth, mars
+
+if __name__ == '__main__':
+    make_planets()
