5373161fe6b9137d3b41465e2149cccccb06b9be
Geoff Coyner <geoffc@qualtrics.com>
2021-02-22 11:34:17 -0800

    added metaclass examples

--- metaclass/tracing.py
+++ metaclass/tracing.py
@@ -33,3 +33,41 @@ class TracingMeta(type):
         print(" kwargs =", kwargs)
         super().__init__(name, bases, namespace)
         print()
+
+
+    def metamethod(cls):
+        print('TracingMeta.metamethod(cls)')
+        print("cls = ", cls)
+        print()
+
+    def __call__(cls, *args, **kwargs):
+        print("TracingMeta.__call__(cls, *args, **kwargs)")
+        print(" cls = ", cls)
+        print(" args = ", args)
+        print(" kwargs = ", kwargs)
+        print(" About to call type.__call__()")
+        obj = super().__call__(*args, **kwargs)
+        print(" Returned from type.__call__()")
+        print("<-- obj =", obj)
+        print()
+        return obj
+
+
+class TracingClass(metaclass=TracingMeta):
+    
+    def __new__(cls, *args, **kwargs):
+        print("TracingClass.__new__(cls, args, kwargs)")
+        print(" cls =", cls)
+        print(" args =", args)
+        print(" kwargs =", kwargs)
+        obj = super().__new__(cls)
+        print("  <-- obj =", obj)
+        print()
+        return obj
+    
+    def __init__(self, *args, **kwargs):
+        print("TracingClass.__init__(self, *args, **kwargs)")
+        print(" self =", self)
+        print(" args =", args)
+        print(" kwargs =", kwargs)
+        print()
