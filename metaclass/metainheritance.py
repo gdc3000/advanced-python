4b18982af73099000121a947a9b83033b440a9ee
Geoff Coyner <geoffc@qualtrics.com>
2021-02-23 11:21:16 -0800

    added metainheritence.py

--- metaclass/metainheritance.py
+++ metaclass/metainheritance.py
@@ -5,10 +5,16 @@ class MetaA(type):
 class MetaB(type):
     pass
 
+class MetaC(MetaA, MetaB):
+    pass
+
 class A(metaclass=MetaA):
     pass
 
 class B(metaclass=MetaB):
+    pass
+
+class C(A, B, metaclass=MetaC):
     pass
 
 class D(A):
