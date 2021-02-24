70ec6117781d8b8551d68d3c5325b9f9062a225d
Geoff Coyner <geoffc@qualtrics.com>
2021-02-24 12:16:02 -0800

    added instancecheck

--- swords/weapons.py
+++ swords/weapons.py
@@ -1,6 +1,38 @@
 class SwordMeta(type):
 
+    def __instancecheck__(cls, instance):
+        return cls.__subclasscheck__(type(instance))
+
     def __subclasscheck__(cls, sub):
         return (hasattr(sub, 'swipe') and callable(sub.swipe)
                 and
                 hasattr(sub, 'sharpen') and callable(sub.sharpen))
+
+
+#Sword plays role of virtual base class
+class Sword(metaclass=SwordMeta):
+    pass
+
+class BroadSword:
+
+    def swipe(self):
+        print("Swoosh!")
+
+    def sharpen(self):
+        print("Shrink!")
+
+
+class SamuraiSword:
+
+    def swipe(self):
+        print("Slice!")
+    
+    def sharpen(self):
+        print("Shrink!")
+
+
+class Rifle:
+
+    def fire(self):
+        print("Bang!")
+
