54d0cd1198d508cf687617af61869678c2b3c71b
Geoff Coyner <geoffc@qualtrics.com>
2021-02-25 12:27:15 -0800

    finished demo of abstractmethod

--- swords/weapons.py
+++ swords/weapons.py
@@ -1,4 +1,4 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 class Sword(ABC):
 
@@ -6,17 +6,36 @@ class Sword(ABC):
     def __subclasshook__(cls, sub):
         return ((hasattr(sub, 'swipe') and callable(sub.swipe)
                 and
+                hasattr(sub, 'thrust') and callable(sub.thrust)
+                and
+                hasattr(sub, 'parry') and callable(sub.parry)
+                and
                 hasattr(sub, 'sharpen') and callable(sub.sharpen))
                 or NotImplemented)
     
+    @abstractmethod
+    def swipe(self):
+        raise NotImplementedError
+
+    @abstractmethod
     def thrust(self):
         print("Thrusting..")
 
+    @abstractmethod
+    def parry(self):
+        raise NotImplementedError
 
-class BroadSword:
+
+class BroadSword(Sword):
 
     def swipe(self):
         print("Swoosh!")
+
+    def thrust(self):
+        super().thrust()
+
+    def parry(self):
+        print("Parry!")
 
     def sharpen(self):
         print("Shrink!")
